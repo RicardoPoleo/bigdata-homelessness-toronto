edd8090b7c71fcb707e090e41f006388d666a8fd
Ricardo Poleo <ricardopoleo@gmail.com>
2024-07-05 20:08:51 -0400

    Updated SocioEconomic links

--- Datasets/SocioEconomicDataset.py
+++ Datasets/SocioEconomicDataset.py
@@ -16,7 +16,15 @@ class SocioEconomicDataset():
         self.urls = [
             self.get_urls_ttc_bus_delays(),
             self.get_urls_ttc_streetcar_delays(),
-            self.get_urls_ttc_subway_delays()
+            self.get_urls_ttc_subway_delays(),
+            self.get_urls_street_furniture_transit_shelter(),
+            self.get_urls_311_service_requests(),
+            self.get_urls_neighborhood_crime_rates(),
+            self.get_urls_neighbourhoods(),
+            self.get_urls_neighbourhood_profiles(),
+            self.get_urls_ward_profiles_25_ward_model(),
+            self.get_urls_ward_profiles_2014_2018_wards(),
+            self.get_urls_ward_profiles_2018_47_ward_model()
         ]
 
     def get_urls_ttc_bus_delays(self):
@@ -32,4 +40,44 @@ class SocioEconomicDataset():
     def get_urls_ttc_subway_delays(self):
         return [
             "https://open.toronto.ca/dataset/ttc-subway-delay-data/"
+        ]
+
+    def get_urls_street_furniture_transit_shelter(self):
+        return [
+            "https://open.toronto.ca/dataset/street-furniture-transit-shelter/"
+        ]
+
+    def get_urls_311_service_requests(self):
+        return [
+            "https://open.toronto.ca/dataset/311-service-requests-customer-initiated/"
+        ]
+
+    def get_urls_neighborhood_crime_rates(self):
+        return [
+            "https://open.toronto.ca/dataset/neighbourhood-crime-rates/"
+        ]
+
+    def get_urls_neighbourhoods(self):
+        return [
+            "https://open.toronto.ca/dataset/neighbourhoods/"
+        ]
+
+    def get_urls_neighbourhood_profiles(self):
+        return [
+            "https://open.toronto.ca/dataset/neighbourhood-profiles/"
+        ]
+
+    def get_urls_ward_profiles_25_ward_model(self):
+        return [
+            "https://open.toronto.ca/dataset/ward-profiles-25-ward-model/"
+        ]
+
+    def get_urls_ward_profiles_2014_2018_wards(self):
+        return [
+            "https://open.toronto.ca/dataset/ward-profiles-2014-2018-wards/"
+        ]
+
+    def get_urls_ward_profiles_2018_47_ward_model(self):
+        return [
+            "https://open.toronto.ca/dataset/ward-profiles-2018-47-ward-model/"
         ]
